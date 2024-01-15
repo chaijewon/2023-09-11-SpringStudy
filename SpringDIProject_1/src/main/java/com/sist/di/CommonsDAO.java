0e11bd99847d03120118391492dc563f59cbb56f
SIST <SIST@DESKTOP-IPQ8TQC>
2024-01-15 13:40:03 +0900

    1111

--- SpringDIProject_1/src/main/java/com/sist/di/CommonsDAO.java
+++ SpringDIProject_1/src/main/java/com/sist/di/CommonsDAO.java
@@ -19,7 +19,7 @@ public class CommonsDAO
 	public void setUrl(String url) {
 		this.url = url;////////
 	}
-	///////////////
+	///////////////////
 	public void setUsername(String username) {
 		this.username = username;
 	}
