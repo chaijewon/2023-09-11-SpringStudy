975ec7f2cea93185de2abfd5badf8fd2f9133035
SIST <SIST@DESKTOP-IPQ8TQC>
2024-01-16 13:54:15 +0900

    111

--- SpringDIProject_5/src/main/java/com/sist/dao/EmpDAO.java
+++ SpringDIProject_5/src/main/java/com/sist/dao/EmpDAO.java
@@ -1,8 +1,7 @@
 package com.sist.dao;
 import java.util.*;
 public class EmpDAO {
-    private EmpMapper mapper;
-
+    private EmpMapper mapper;//
 	public void setMapper(EmpMapper mapper) {
 		this.mapper = mapper;
 	}
